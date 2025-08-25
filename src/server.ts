58e9d02bba4a2e39b59a309e9e7b6ad6d2d41463
Rana <reduanulislam92665@gmail.com>
2025-08-25 06:03:09 +0600

    server implement again

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
+
 import mongoose from 'mongoose';
 import app from './app';
 import { envVars } from './app/config/env';
 
-// ✅ Only connect to MongoDB, no app.listen()
+const port = envVars.port;
+
 async function main() {
   try {
-    if (!mongoose.connection.readyState) {
-      await mongoose.connect(envVars.database_url);
-      console.log('🛢️ Connected to MongoDB');
-    }
+   await mongoose.connect(envVars.database_url);
+
+    console.log('🛢️ Connected to MongoDB');
+
+    app.listen(port, () => {
+      console.log(`🚀 Server running at http://localhost:${port}`);
+    });
   } catch (err) {
     console.error('❌ MongoDB connection error:', err);
     process.exit(1); // Exit if DB fails to connect
   }
 }
 
-main();
 
-export default app; // ✅ Vercel will use this
+main();
